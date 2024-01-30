60a2d17bf91ebf4bb206288db07d3e5ff69674f1
Thiel <patrickthiel@outlook.de>
2024-01-30 10:39:15 +0100

    Create make_consistent_file_naming.R
    Function to ensure consistent file naming for the original data for various deliveries.

---
+++ code/make_consistent_file_naming.R
@@ -0,0 +1,67 @@
+make_consistent_file_naming <- function(current_delivery = NA) {
+    #' @title Make consistent file naming
+    #' 
+    #' @description This function guarantees consistent file naming for the next
+    #' steps.
+    #' 
+    #' @param current_delivery The current delivery.
+    #' 
+    #' @return NULL
+    #' @author Patrick Thiel
+    
+    #----------------------------------------------
+    # special handling of delivery 2312 since it includes all past data (2010 to
+    # 2022)
+    
+    if (current_delivery == "Lieferung_2312") {
+        # list all folders
+        file_list <- list.files(
+            file.path(
+                paths()[["data_path"]],
+                "original",
+                current_delivery
+            ),
+            pattern = "*.csv$"
+        )
+
+        # data storage
+        data_storage <- list()
+
+        # read all files
+        for (file in file_list) {
+            dta <- data.table::fread(
+                file.path(
+                    paths()[["data_path"]],
+                    "original",
+                    current_delivery,
+                    file,
+                    file
+                )
+            )
+
+            data_storage[[file]] <- dta
+        }
+
+        # combine all files
+        all_data <- data.table::rbindlist(
+            data_storage,
+            fill = TRUE
+        )
+    
+        # export
+        data.table::fwrite(
+            all_data,
+            file.path(
+                paths()[["data_path"]],
+                "original",
+                current_delivery,
+                "commercial_data_all.csv"
+            )
+        )
+    }
+
+    #----------------------------------------------
+    # return
+
+    return(NULL)
+}
