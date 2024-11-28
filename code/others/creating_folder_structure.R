703f57b0f682978e87865aa6d79500ddd4d6e3d6
Patrick Thiel <patrickthiel@outlook.de>
2024-11-28 14:06:49 +0100

    Create creating_folder_structure.R
    Function to create the data folder structure for the new version.

---
+++ code/others/creating_folder_structure.R
@@ -0,0 +1,65 @@
+creating_folder_structure <- function() {
+    #' @title Create folder structure
+    #' 
+    #' @description This function creates the folder structure for the output
+    #' of a new wave.
+    #' 
+    #' @return NULL
+    #' @author Patrick Thiel
+    #' 
+    #----------------------------------------------
+    # folder generation for new delivery (in data folder)
+
+    for (data_folder in config_globals()[["data_folders"]]) {
+        if (data_folder == "processed") {
+            ifelse(
+                !dir.exists(
+                    file.path(
+                        config_paths()[["data_path"]],
+                        data_folder,
+                        config_globals()[["current_delivery"]]
+                    )
+                ),
+                yes = dir.create(
+                    file.path(
+                        config_paths()[["data_path"]],
+                        data_folder,
+                        config_globals()[["current_delivery"]]
+                    )
+                ),
+                no = cli::cli_alert_success(
+                    col_green(
+                        "Delivery directory for \"{data_folder}\" data folder already exists."    
+                    )
+                )
+            )
+        } else {
+            ifelse(
+                !dir.exists(
+                    file.path(
+                        config_paths()[["data_path"]],
+                        data_folder,
+                        config_globals()[["current_version"]]
+                    )
+                ),
+                yes = dir.create(
+                    file.path(
+                        config_paths()[["data_path"]],
+                        data_folder,
+                        config_globals()[["current_version"]]
+                    )
+                ),
+                no = cli::cli_alert_success(
+                    col_green(
+                        "Version directory for \"{data_folder}\" data folder already exists."    
+                    )
+                )
+            )
+        }
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(NULL)
+}
