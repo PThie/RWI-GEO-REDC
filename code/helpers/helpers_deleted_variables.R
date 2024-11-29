fc1b05385157752f8a43526233f5c2dd0643ac2e
Patrick Thiel <patrickthiel@outlook.de>
2024-11-29 11:11:20 +0100

    Create helpers_deleted_variables.R
    Function to store the variables that are suppose to be deleted during the cleaning process.

---
+++ code/helpers/helpers_deleted_variables.R
@@ -0,0 +1,44 @@
+helpers_deleted_variables <- function() {
+    #' @title Define variables to be removed
+    #' 
+    #' @description This function defines the variables to be removed from the
+    #' data set that are potentially in the data set because the data were
+    # delivered with the residential data together
+    #'
+    #' @return Vector with variables to be removed
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # define variables to be removed
+    # TODO NEW WAVE: Update variables to be removed (if necessary)
+    
+    del_vars <- c(
+        "mieteinnahmenpromonat",
+        "schlafzimmer",
+        "badezimmer",
+        "gaestewc",
+        "einliegerwohnung",
+        "ferienhaus",
+        "einbaukueche",
+        "balkon",
+        "garten",
+        "foerderung",
+        "parkplatzpreis",
+        "wohngeld",
+        "is24_stadt_kreis", # dropped in RED as well
+        "kreis",
+        "is24_bezirk_gemeinde", # dropped in RED as well
+        "is24_bundesland", # dropped in RED as well
+        "skid", # dropped in RED as well   
+        "bgid", # dropped in RED as well
+        "objekt_beschreibung", # text description of the add removed for now
+        "betreut", # not used anymore
+        "nebenraeume", # not used in REDC
+        "einstelldatum" # not needed for now
+    )
+
+    #--------------------------------------------------
+    # return
+    
+    return(del_vars)
+}
