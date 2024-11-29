5d4a4a3d063bdf4cf8ef9988ecea976b03bf46dd
Patrick Thiel <patrickthiel@outlook.de>
2024-11-29 15:47:41 +0100

    Create helpers_target_names.R
    Function for naming the different targets object used in tar_eval statements.

---
+++ code/helpers/helpers_target_names.R
@@ -0,0 +1,34 @@
+helpers_target_names <- function() {
+    #' @title Create target names
+    #' 
+    #' @description This function creates a list of target names used in the
+    #' pipeline when dynamic branching is used (i.e. when tar_eval is used).
+    #'  
+    #' @return List, target names
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # list of target names
+
+    # NOTE: define outside list to be able to use in list
+    spatial_units_english = c("district", "municipality", "zip_code")
+
+    target_names <- list(
+        #--------------------------------------------------
+        # names for the spatial information
+        # NOTE: order has to match either other
+        "spatial_units_german" = c("Kreis", "Gemeinde", "PLZ"),
+        "ags_names" = c("kid2019", "gid2019", "plz2019"),
+        # NOTE: zip code does not have a GEN name
+        "gen_names" = c("kname2019", "gname2019", NA),
+        # name for the target objects
+        "spatial_data" = glue::glue(
+            "spatial_data_{spatial_units_english}"
+        )
+    )
+    
+    #--------------------------------------------------
+    # return
+
+    return(target_names)
+}
