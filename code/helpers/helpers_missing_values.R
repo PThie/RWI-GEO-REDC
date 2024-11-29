c19e0bd3a67d72e76e3a5fcea57975b7beb63fd6
Patrick Thiel <patrickthiel@outlook.de>
2024-11-29 12:29:29 +0100

    Create helpers_missing_values.R
    Function that classifies the different missing types.

---
+++ code/helpers/helpers_missing_values.R
@@ -0,0 +1,24 @@
+helpers_missing_values <- function() {
+    #' @title Define missing values
+    #' 
+    #' @description This function defines the missing values for the data set.
+    #' 
+    #' @return List of missing values
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # define missing values
+
+    missings <- list(
+        "implausible" = -5,
+        "not_used_anymore" = -6,
+        "not_specified" = -7,
+        "other" = -9,
+        "immo_missing" = -1 # how ImmoScout defines missings
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(missings)
+}
