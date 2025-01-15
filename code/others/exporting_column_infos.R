e24804af5c16e56da830b6c8f420755f4531a864
Patrick Thiel <patrickthiel@outlook.de>
2025-01-15 09:45:00 +0100

    Create exporting_column_infos.R
    Closes #19;
    
    This function exports the column names and types of the very first delivery to check for the consistency in variables in later deliveries.

---
+++ code/others/exporting_column_infos.R
@@ -0,0 +1,62 @@
+exporting_column_infos <- function(
+    housing_data = NA
+) {
+    #' @title Exporting column infos of first delivery
+    #' 
+    #' @description This function exports the column names and types of the
+    #' first delivery as benchmark to check for consistency in the following
+    #' deliveries.
+    #' 
+    #' @param housing_data Dataframe with original housing data
+    #' 
+    #' @return Dataframe with column names and types
+    #' @author Patrick Thiel 
+
+    #--------------------------------------------------
+    # export column names and types only for the very first delivery
+    # (= benchmark)
+
+    if (config_globals()[["current_delivery"]] == "Lieferung_2306") {
+        #--------------------------------------------------
+        # extract column types and names
+
+        coltypes <- sapply(housing_data, class) |>
+            unlist() |>
+            as.data.frame() |>
+            dplyr::rename(columns_types = 1)
+
+        coltypes$columns <- rownames(coltypes)
+        rownames(coltypes) <- NULL
+
+        coltypes <- coltypes |>
+            dplyr::relocate(columns)
+
+        #--------------------------------------------------
+        # export
+
+        data.table::fwrite(
+            coltypes,
+            file.path(
+                config_paths()[["output_path"]],
+                config_globals()[["current_version"]],
+                "column_types.csv"
+            )
+        )
+    }
+
+    #--------------------------------------------------
+    # re-read column types and names of the first delivery
+
+    coltypes <- data.table::fread(
+        file.path(
+            config_paths()[["output_path"]],
+            config_globals()[["current_version"]],
+            "column_types.csv"
+        )
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(coltypes)
+}
