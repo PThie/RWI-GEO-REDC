9fb9ae58bfa27a4bfdac5588a062ad505e7816a8
Patrick Thiel <patrickthiel@outlook.de>
2025-01-23 14:20:41 +0100

    Expansion of time horizon
    Typically there is a delay in the microm data such that the housing data is more recent. As consequence, in order to use the microm data in the anonymization, one has to expand the microm data (similar to expanding at the beginning).

--- code/clean/cleaning_microm_data.R
+++ code/clean/cleaning_microm_data.R
@@ -24,7 +24,7 @@ cleaning_microm_data <- function(
     #--------------------------------------------------
     # create average value for years 2005 and 2009 to fill years 2007 to 2008
 
-    avg_microm <- microm_data_prep |>
+    avg_microm_beginning <- microm_data_prep |>
         dplyr::filter(year %in% c(2005, 2009)) |>
         dplyr::group_by(ergg_1km) |>
         dplyr::summarise(
@@ -36,21 +36,77 @@ cleaning_microm_data <- function(
     #--------------------------------------------------
     # create dataframe containing years 2007 to now
 
-    microm_complete <- rbind(
+    microm_beginning <- rbind(
         microm_data_prep |>
             dplyr::filter(year != 2005) |>
             dplyr::mutate(
                 num_business = as.numeric(num_business)
             ),
-        avg_microm |>
+        avg_microm_beginning |>
             dplyr::mutate(
                 year = 2007
             ),
-        avg_microm |>
+        avg_microm_beginning |>
             dplyr::mutate(
                 year = 2008
             )
     )
+
+    #--------------------------------------------------
+    # create average values for years not yet in microm data
+
+    avg_microm_end <- microm_data_prep |>
+        dplyr::filter(
+            year %in% c(
+                config_globals()[["microm_max_year"]] - 1,
+                config_globals()[["microm_max_year"]]
+            )
+        ) |>
+        dplyr::group_by(ergg_1km) |>
+        dplyr::summarise(
+            num_business = mean(num_business, na.rm = TRUE)
+        ) |>
+        dplyr::ungroup() |>
+        as.data.frame()
+
+    #--------------------------------------------------
+    # duplicate data for end periods
+
+    # determine years for duplication
+    end_years <- seq(
+        config_globals()[["microm_max_year"]] + 1,
+        config_globals()[["max_year"]],
+        by = 1
+    )
+
+    # duplicate data
+    end_years_data_list <- list()
+    for (year in end_years) {
+        end_years_data_list[[year]] <- avg_microm_end |>
+            dplyr::mutate(
+                year = year
+            )
+    }
+
+    # combine all
+    end_years_data <- data.table::rbindlist(end_years_data_list)
+
+    #--------------------------------------------------
+    # merge all datasets
+
+    # determine if housing data and microm have different time horizons
+    difference_years <- config_globals()[["max_year"]] -
+        config_globals()[["microm_max_year"]]
+
+    # merge all datasets
+    if (difference_years > 0) {
+        microm_complete <- rbind(
+            microm_beginning,
+            end_years_data
+        )
+    } else {
+        microm_complete <- microm_beginning
+    }
 
     #--------------------------------------------------
     # add dummy for censoring
@@ -61,7 +117,9 @@ cleaning_microm_data <- function(
             censor_grid_id = dplyr::case_when(
                 num_business < config_globals()[["censoring_threshold_businesses"]] ~ 1,
                 TRUE ~ 0
-            )
+            ),
+            # make year integer to match housing data
+            year = as.integer(year)
         ) |>
         dplyr::select(-num_business)
 
