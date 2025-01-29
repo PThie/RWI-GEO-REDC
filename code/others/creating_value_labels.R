7191bbc5af02577d528add17772a0528f5f9a1de
Patrick Thiel <patrickthiel@outlook.de>
2025-01-29 09:47:40 +0100

    Create creating_value_labels.R
    Function to create and export the labels for values of categorical variables.
    
    Related to #26

---
+++ code/others/creating_value_labels.R
@@ -0,0 +1,243 @@
+creating_value_labels <- function(
+    housing_data = NA,
+    variable_labels = NA
+) {
+    #' @title Create value labels for categorical variables
+    #' 
+    #' @description This function creates value labels for categorical variables.
+    #' 
+    #' @param housing_data Dataframe with housing data.
+    #' @param variable_labels Dataframe with variable labels.
+    #' 
+    #' @return Dataframe with value labels for categorical variables.
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # subset housing data for categorical variables
+
+    categorical_data <- housing_data |>
+        dplyr::select(
+            variable_labels |>
+                dplyr::filter(`variable type` == "categorical") |>
+                dplyr::pull(variable)
+        )
+
+    # extract variable names
+    var_names <- sort(names(categorical_data))
+
+    #--------------------------------------------------
+    # create vector of variable names (according to length of unique values)
+    # create vector of unique values
+
+    var_names_rep <- c()
+    unique_values <- c()
+
+    for (var in var_names) {
+        var_names_rep <- c(
+            var_names_rep,
+            rep(var, helpers_unique_categories()[[var]]$unique_values)
+        )
+
+        # NOTE: dummies and dupID_gen start at 0
+        if (helpers_unique_categories()[[var]]$type == "dummy" | var == "dupID_gen") {
+            unique_values <- c(
+                unique_values,
+                seq(0, helpers_unique_categories()[[var]]$unique_values - 1)
+            )
+        } else {
+            unique_values <- c(
+                unique_values,
+                seq(1, helpers_unique_categories()[[var]]$unique_values)
+            )
+        }
+    }
+
+    # combine into dataframe
+    value_labels <- data.frame(
+        variable = var_names_rep,
+        value = unique_values
+    )
+
+    #--------------------------------------------------
+    # add labels to the value labels
+
+    value_labels <- value_labels |>
+        dplyr::mutate(
+            label = NA_character_,
+            label = dplyr::case_when(
+                #--------------------------------------------------
+                variable == "basement" & value == 0 ~ "No",
+                variable == "basement" & value == 1 ~ "Yes",
+                #--------------------------------------------------
+                variable == "category_business" & value == 1 ~ "Office and commercial buildings",
+                variable == "category_business" & value == 2 ~ "Store",
+                variable == "category_business" & value == 3 ~ "Living and commercial buildings",
+                variable == "category_business" & value == 4 ~ "Sales area",
+                variable == "category_business" & value == 5 ~ "Office building",
+                variable == "category_business" & value == 6 ~ "Office floors",
+                variable == "category_business" & value == 7 ~ "Special property",
+                variable == "category_business" & value == 8 ~ "Office center",
+                variable == "category_business" & value == 9 ~ "Exhibition space",
+                variable == "category_business" & value == 10 ~ "Restaurant",
+                variable == "category_business" & value == 11 ~ "Estate",
+                variable == "category_business" & value == 12 ~ "Shopping center",
+                variable == "category_business" & value == 13 ~ "Cafe",
+                variable == "category_business" & value == 14 ~ "Guesthouse",
+                variable == "category_business" & value == 15 ~ "Leisure facility",
+                variable == "category_business" & value == 16 ~ "Commercial center",
+                variable == "category_business" & value == 17 ~ "Hotel",
+                variable == "category_business" & value == 18 ~ "Bar service and Lounge",
+                variable == "category_business" & value == 19 ~ "Riding school",
+                variable == "category_business" & value == 20 ~ "Warehouse with open space",
+                variable == "category_business" & value == 21 ~ "Forwarding warehouse",
+                variable == "category_business" & value == 22 ~ "High-bay warehouse",
+                variable == "category_business" & value == 23 ~ "Club and discotheque",
+                variable == "category_business" & value == 24 ~ "Cold storage",
+                variable == "category_business" & value == 25 ~ "Department store",
+                variable == "category_business" & value == 26 ~ "Factory outlet",
+                variable == "category_business" & value == 27 ~ "Vacation bungalow",
+                variable == "category_business" & value == 28 ~ "Practice",
+                variable == "category_business" & value == 29 ~ "Office",
+                variable == "category_business" & value == 30 ~ "Hall",
+                variable == "category_business" & value == 31 ~ "Industrial hall",
+                variable == "category_business" & value == 32 ~ "Office and warehouse buildings",
+                variable == "category_business" & value == 33 ~ "Commercial space",
+                variable == "category_business" & value == 34 ~ "Storage space",
+                variable == "category_business" & value == 35 ~ "Warehouse",
+                variable == "category_business" & value == 36 ~ "Workshop",
+                variable == "category_business" & value == 37 ~ "Service area",
+                variable == "category_business" & value == 38 ~ "Inn/ Tavern",
+                variable == "category_business" & value == 39 ~ "Industrial hall with open space",
+                variable == "category_business" & value == 40 ~ "Loft",
+                variable == "category_business" & value == 41 ~ "Self-service market",
+                variable == "category_business" & value == 42 ~ "Practice floor",
+                variable == "category_business" & value == 43 ~ "Lodging house",
+                variable == "category_business" & value == 44 ~ "Studio",
+                variable == "category_business" & value == 45 ~ "Sales hall",
+                variable == "category_business" & value == 46 ~ "Practice building",
+                variable == "category_business" & value == 48 ~ "Hotel property",
+                variable == "category_business" & value == 49 ~ "Business park",
+                variable == "category_business" & value == 47 ~ "Kiosk",
+                variable == "category_business" & value == 50 ~ "Farm",
+                variable == "category_business" & value == 51 ~ "Hotel garni",
+                variable == "category_business" & value == 52 ~ "Refrigerated warehouse",
+                variable == "category_business" & value == 53 ~ "Winery",
+                #--------------------------------------------------
+                variable == "dupID_gen" & value == 0 ~ "First occurence of the ID",
+                variable == "dupID_gen" & value == 1 ~ "Identical to 1 and time difference is smaller/ equal 6 months",
+                variable == "dupID_gen" & value == 2 ~ "Identical to 1 and time difference is greater 6 months",
+                variable == "dupID_gen" & value == 3 ~ "Differences in set 2 variables (below thresholds) and time difference is smaller/ equal 6 months",
+                variable == "dupID_gen" & value == 4 ~ "Differences in set 2 variables (below thresholds) and time difference is greater 6 months",
+                variable == "dupID_gen" & value == 5 ~ "Differences in set 1 variables (below thresholds) and time difference is smaller/ equal 6 months",
+                variable == "dupID_gen" & value == 6 ~ "Differences in set 1 variables (below thresholds) and time difference is greater 6 months",
+                variable == "dupID_gen" & value == 7 ~ "Differences in set 1 and 2 variables (below thresholds) and time difference is smaller/ equal 6 months",
+                variable == "dupID_gen" & value == 8 ~ "Differences in set 1 and 2 variables (below thresholds) and time difference is greater 6 months",
+                variable == "dupID_gen" & value == 9 ~ "Differences in set 1 and 2 variables (above thresholds)",
+                #--------------------------------------------------
+                variable == "elevator" & value == 0 ~ "No",
+                variable == "elevator" & value == 1 ~ "Yes",
+                #--------------------------------------------------
+                variable == "endowment" & value == 1 ~ "Simple",
+                variable == "endowment" & value == 2 ~ "Normal",
+                variable == "endowment" & value == 3 ~ "Sophisticated",
+                variable == "endowment" & value == 4 ~ "Deluxe",
+                #--------------------------------------------------
+                variable == "energy_certificate_type" & value == 1 ~ "Energy demand",
+                variable == "energy_certificate_type" & value == 2 ~ "Energy usage",
+                #--------------------------------------------------
+                variable == "energy_efficiency_class" & value == 1 ~ "APLUS",
+                variable == "energy_efficiency_class" & value == 2 ~ "A",
+                variable == "energy_efficiency_class" & value == 3 ~ "B",
+                variable == "energy_efficiency_class" & value == 4 ~ "C",
+                variable == "energy_efficiency_class" & value == 5 ~ "D",
+                variable == "energy_efficiency_class" & value == 6 ~ "E",
+                variable == "energy_efficiency_class" & value == 7 ~ "F",
+                variable == "energy_efficiency_class" & value == 8 ~ "G",
+                variable == "energy_efficiency_class" & value == 9 ~ "H",
+                #--------------------------------------------------
+                variable == "heating_type" & value == 1 ~ "Cogeneration or combined heat and power plant",
+                variable == "heating_type" & value == 2 ~ "Electric heating",
+                variable == "heating_type" & value == 3 ~ "Self-contained central heating",
+                variable == "heating_type" & value == 4 ~ "District heating",
+                variable == "heating_type" & value == 5 ~ "Floor heating",
+                variable == "heating_type" & value == 6 ~ "Gas heating",
+                variable == "heating_type" & value == 7 ~ "Wood pellet heating",
+                variable == "heating_type" & value == 8 ~ "Night storage heaters",
+                variable == "heating_type" & value == 9 ~ "Heating by stove",
+                variable == "heating_type" & value == 10 ~ "Oil heating",
+                variable == "heating_type" & value == 11 ~ "Solar heating",
+                variable == "heating_type" & value == 12 ~ "Thermal heat pump",
+                variable == "heating_type" & value == 13 ~ "Central pump",
+                #--------------------------------------------------
+                variable == "parking" & value == 0 ~ "No",
+                variable == "parking" & value == 1 ~ "Yes",
+                #--------------------------------------------------
+                variable == "property_condition" & value == 1 ~ "First occupancy",
+                variable == "property_condition" & value == 2 ~ "First occupancy after refurbishment",
+                variable == "property_condition" & value == 3 ~ "Like new",
+                variable == "property_condition" & value == 4 ~ "Refurbished",
+                variable == "property_condition" & value == 5 ~ "Modernized",
+                variable == "property_condition" & value == 6 ~ "Completely renovated",
+                variable == "property_condition" & value == 7 ~ "Well-kept",
+                variable == "property_condition" & value == 8 ~ "Need of renovation",
+                variable == "property_condition" & value == 9 ~ "By arrangement",
+                variable == "property_condition" & value == 10 ~ "Dilapidated",
+                #--------------------------------------------------
+                variable == "property_type" & value == 1 ~ "Office and practice",
+                variable == "property_type" & value == 2 ~ "Retail",
+                variable == "property_type" & value == 3 ~ "Halls and production",
+                variable == "property_type" & value == 4 ~ "Special trades",
+                variable == "property_type" & value == 5 ~ "Gastronomy and hotel",
+                #--------------------------------------------------
+                variable == "protected_building" & value == 0 ~ "No",
+                variable == "protected_building" & value == 1 ~ "Yes",
+                #--------------------------------------------------
+                variable == "provider" & value == 1 ~ "Private",
+                variable == "provider" & value == 2 ~ "Broker",
+                variable == "provider" & value == 3 ~ "Housing industry",
+                variable == "provider" & value == 4 ~ "Property developer",
+                variable == "provider" & value == 5 ~ "Financial institution",
+                variable == "provider" & value == 6 ~ "Commercial provider",
+                variable == "provider" & value == 7 ~ "House construction",
+                variable == "provider" & value == 8 ~ "Relocation",
+                #--------------------------------------------------
+                variable == "security_deposit_type" & value == 1 ~ "Cold rent",
+                variable == "security_deposit_type" & value == 2 ~ "Warm rent",
+                variable == "security_deposit_type" & value == 3 ~ "Other rent (cold or warm)",
+                variable == "security_deposit_type" & value == 4 ~ "Miscellaneous (insurance, credit, gurantee)",
+                variable == "security_deposit_type" & value == 5 ~ "Price",
+                variable == "security_deposit_type" & value == 6 ~ "No deposit",
+                #--------------------------------------------------
+                variable == "wheelchair_accessible" & value == 0 ~ "No",
+                variable == "wheelchair_accessible" & value == 1 ~ "Yes"
+            )
+        )
+
+    # Check for no NA in values (otherwise missed to specify a label)
+    targets::tar_assert_true(
+        length(which(is.na(value_labels$value))) == 0,
+        msg = glue::glue(
+            "!!! WARNING: ",
+            "Variable(s) have been missed to specify a label.",
+            " (Error code: cvl#1)"
+        )
+    )
+
+    #--------------------------------------------------
+    # export
+
+    openxlsx::write.xlsx(
+        value_labels,
+        file.path(
+            config_paths()[["output_path"]],
+            config_globals()[["current_version"]],
+            "value_labels.xlsx"
+        ),
+        rowNames = FALSE
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(value_labels)
+}
