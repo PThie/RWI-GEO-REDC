ec796dc73b3bf44f9296d1fc5aa45aa29e2a3682
Patrick Thiel <patrickthiel@outlook.de>
2025-02-03 16:38:49 +0100

    Create testing_suf_compliance.R
    Function that tests whether all sensitive columns have been removed in the exported SUF data.

---
+++ src/test/testing_suf_compliance.R
@@ -0,0 +1,34 @@
+testing_suf_compliance <- function(
+    suf_data = NA,
+    file_format = NA
+) {
+    #' @title Test SUF compliance
+    #' 
+    #' @description This function tests if the SUF data is compliant with the
+    #' requirements, i.e. if all sensitive variables have been deleted.
+    #' 
+    #' @param suf_data Dataframe with the SUF data
+    #' @param file_format File format of the SUF data (character)
+    #' 
+    #' @return NULL
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # test if all variables have been deleted that are sensitive
+
+    for (col in helpers_deleted_variables_SUF()) {
+        targets::tar_assert_true(
+            !(col %in% names(suf_data)),
+            msg = glue::glue(
+                "!!! WARNING: ",
+                "Sensitive variable '{col}' is still present in the {file_format} data.",
+                " (Error code: sct#1)"
+            )
+        )
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(NULL)
+}
