153392fed8f89f5b97ccb743f5b0fc968f2d128b
Patrick Thiel <patrickthiel@outlook.de>
2025-02-03 12:07:31 +0100

    Create helpers_translations.R
    Function that lists the translations of the variable names from German to English.

---
+++ code/helpers/helpers_translations.R
@@ -0,0 +1,55 @@
+helpers_translations <- function() {
+    #' @title Translation of variable names
+    #'
+    #' @description This function lists the translated variable names from the
+    #' original data to the English version.
+    #' 
+    #' @return Vector of translated variable names
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # Define translations
+    # NOTE: Order has to match the order of translate_variables
+
+    translations <- c(
+        "freiab" = "available_from",
+        "plz" = "zipcode",
+        "strasse" = "street",
+        "hausnr" = "house_number",
+        "immobilientyp" = "property_type",
+        "objektzustand" = "property_condition",
+        "ausstattung" = "endowment",
+        "mietekalt" = "cold_rent",
+        "nebenkosten" = "ancillary_costs",
+        "courtage" = "commission",
+        "kaufpreis" = "listing_price",
+        "baujahr" = "construction_year",
+        "letzte_modernisierung" = "last_modernization",
+        "grundstuecksflaeche" = "plot_area",
+        "nutzflaeche" = "usable_area",
+        "etage" = "floor",    
+        "denkmalobjekt" = "protected_building",
+        "aufzug" = "elevator",
+        "keller" = "basement",
+        "parkplatz" = "parking",
+        "rollstuhlgerecht" = "wheelchair_accessible",
+        "heizungsart" = "heating_type",
+        "energieausweistyp" = "energy_certificate_type",
+        "ev_kennwert" = "energy_consumption_index",
+        "energieeffizienzklasse" = "energy_efficiency_class",
+        "laufzeittage" = "duration_days",
+        "miete_proqm" = "rent_per_sqm",
+        "teilbar_ab" = "divisible_from",
+        "anbieter" = "provider",
+        "kategorie_business" = "category_business",
+        "nebenkosten_proqm" = "ancillary_costs_per_sqm",
+        "mietekaution_price" = "security_deposit_price",
+        "mietekaution_type" = "security_deposit_type",
+        "mietekaution_months" = "security_deposit_months"
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(translations)
+}
