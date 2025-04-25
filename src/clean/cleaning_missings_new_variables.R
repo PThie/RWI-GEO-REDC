63d07614715f8782e264f9fc5ff43cea5914f952
Patrick Thiel <patrickthiel@outlook.de>
2025-04-25 13:45:35 +0200

    Handle types in missing assignment
    Since not all new variables are numeric, I introduced the case of character variables to assign the proper type for the missings.

--- src/clean/cleaning_missings_new_variables.R
+++ src/clean/cleaning_missings_new_variables.R
@@ -11,24 +11,39 @@ cleaning_missings_new_variables <- function(housing_data = NA) {
     #--------------------------------------------------
     # replace missing values with appropriate encoding
 
-    if (config_globals()[["current_delivery"]] == "Lieferung_2312") {
-        for (var in config_new_variables()[["2312"]]) {
-            # check that new variables are indeed always missing in the first delivery
-            first_delivery <- housing_data |>
-                dplyr::filter(redc_delivery == 1)
+    del <- stringr::str_extract(
+        config_globals()[["current_delivery"]],
+        "[0-9]+"
+    )
 
-            targets::tar_assert_true(
-                all(
-                    unique(first_delivery[[var]]) %in% c(NA)
-                ),
-                msg = glue::glue(
-                    "!!! WARNING: ",
-                    "Variable {var} is not missing in first delivery.",
-                    " (Error code: cmnv#1)"
+    for (var in config_new_variables()[[del]]) {
+        # check that new variables are indeed always missing in the first delivery
+        first_delivery <- housing_data |>
+            dplyr::filter(redc_delivery == 1)
+
+        targets::tar_assert_true(
+            all(
+                unique(first_delivery[[var]]) %in% c(NA)
+            ),
+            msg = glue::glue(
+                "!!! WARNING: ",
+                "Variable {var} is not missing in first delivery.",
+                " (Error code: cmnv#1)"
+            )
+        )
+
+        # replace with appropriate missing value
+        if (typeof(housing_data[[var]]) == "character") {
+        housing_data <- housing_data |>
+            dplyr::mutate(
+                !!rlang::sym(var) := dplyr::case_when(
+                    is.na(!!rlang::sym(var)) & redc_delivery == 1 ~ as.character(
+                        helpers_missing_values()[["not_used_anymore"]]
+                    ),
+                    TRUE ~ !!rlang::sym(var)
                 )
             )
-
-            # replace with appropriate missing value
+        } else {
             housing_data <- housing_data |>
                 dplyr::mutate(
                     !!rlang::sym(var) := dplyr::case_when(
@@ -36,25 +51,25 @@ cleaning_missings_new_variables <- function(housing_data = NA) {
                         TRUE ~ !!rlang::sym(var)
                     )
                 )
-
-            # check that the transformation worked
-            num_missings <- length(which(is.na(first_delivery[[var]])))
-            num_replaced <- length(which(
-                housing_data |>
-                    dplyr::filter(redc_delivery == 1) |>
-                    dplyr::select(var) == helpers_missing_values()[["not_used_anymore"]]
-            ))
-
-            targets::tar_assert_true(
-                num_missings == num_replaced,
-                msg = glue::glue(
-                    "!!! WARNING: ",
-                    "Variable transformation for {var} did not work.",
-                    " (Error code: cmnv#2)"
-                )
-            )
         }
-    } 
+
+        # check that the transformation worked
+        num_missings <- length(which(is.na(first_delivery[[var]])))
+        num_replaced <- length(which(
+            housing_data |>
+                dplyr::filter(redc_delivery == 1) |>
+                dplyr::select(var) == helpers_missing_values()[["not_used_anymore"]]
+        ))
+
+        targets::tar_assert_true(
+            num_missings == num_replaced,
+            msg = glue::glue(
+                "!!! WARNING: ",
+                "Variable transformation for {var} did not work.",
+                " (Error code: cmnv#2)"
+            )
+        )
+    }
 
     #--------------------------------------------------
     # return
