dd235451cbd4d4101c4ef77f92075d6936d3921c
Patrick Thiel <patrickthiel@outlook.de>
2025-06-18 09:20:15 +0200

    Switched to quarterly trends
    Used quarter price developments instead of monthly values for better clarity in the figure (i.e., less ticks on the x-axis but also removing some of the natural fluctuation of the raw data).

--- src/test/testing_temporal_trends.R
+++ src/test/testing_temporal_trends.R
@@ -23,7 +23,8 @@ testing_temporal_trends <- function(
     housing_data_prep <- housing_data |>
         dplyr::mutate(
             year_month = paste0(ejahr, "-", emonat),
-            year_month = zoo::as.yearmon(year_month, "%Y-%m")
+            year_month = zoo::as.yearmon(year_month, "%Y-%m"),
+            year_quarter = zoo::as.yearqtr(year_month)
         )
 
     #--------------------------------------------------
@@ -47,7 +48,7 @@ testing_temporal_trends <- function(
         dplyr::mutate(
             listing_price_sqm = listing_price / usable_area
         ) |>
-        dplyr::group_by(year_month) |>
+        dplyr::group_by(year_quarter) |>
         dplyr::summarize(
             listing_price_sqm = mean(listing_price_sqm, na.rm = TRUE),
             rent_per_sqm = mean(rent_per_sqm, na.rm = TRUE)
@@ -60,7 +61,20 @@ testing_temporal_trends <- function(
     linewidth_local <- 1.2
     size_local <- 3
 
-    lineplot <- ggplot(data = housing_data_prices, aes(x = year_month))+
+    brks <- c(
+        unique(housing_data_prices$year_quarter)[
+            stringr::str_detect(housing_data_prices$year_quarter, "Q1")
+        ],
+        zoo::as.yearqtr(paste(
+            config_globals()[["max_year"]] + 1,
+            "Q1"
+        ))
+    )
+
+    lineplot <- ggplot(
+        data = housing_data_prices,
+        aes(x = year_quarter)
+    )+
         geom_line(
             mapping = aes(
                 y = listing_price_sqm,
@@ -80,7 +94,7 @@ testing_temporal_trends <- function(
                 y = rent_per_sqm * 150,
                 color = "rents"
             ),
-            size = linewidth_local
+            linewidth = linewidth_local
         )+
         geom_point(
             mapping = aes(
@@ -100,9 +114,9 @@ testing_temporal_trends <- function(
             limits = c(0, max(housing_data_prices$listing_price_sqm) + 1000),
             labels = scales::comma
         )+
-        zoo::scale_x_yearmon(
-            labels = scales::label_date(format = "%Y-%m"),
-            breaks = housing_data_prices$year_month,
+        zoo::scale_x_yearqtr(
+            labels = scales::label_date(format = "%Y-%q"),
+            breaks = brks,
             name = ""
         )+
         scale_color_manual(
