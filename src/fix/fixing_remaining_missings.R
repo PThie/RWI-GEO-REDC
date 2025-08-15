047aaa1c51421210fbf7b2a822a4ec7e0edc44d5
heuer-felix <felix-heuer@gmx.de>
2025-08-15 12:12:22 +0200

    Update fixing_remaining_missings.R
    apply function also to Lieferung_2406

--- src/fix/fixing_remaining_missings.R
+++ src/fix/fixing_remaining_missings.R
@@ -14,7 +14,7 @@ fixing_remaining_missings <- function(
     #--------------------------------------------------
     # handle delivery specific missing values
 
-    if (config_globals()[["current_delivery"]] == "Lieferung_23121") {
+    if (config_globals()[["current_delivery"]] %in% c("Lieferung_23121", "Lieferung_2406")) {
         #--------------------------------------------------
         # handle variables that are not always present
         # NOTE: these variables are introduced in the second delivery (delivery 2312)
