dd2dd60e816bb79c925c34a54ba0991f5c9cf9ae
heuer-felix <felix-heuer@gmx.de>
2026-01-26 12:24:32 +0100

    Update cleaning_missings_new_variables.R
    fix test for missing values in new variable. Allows code to run is variable are not available in first delivery and only some later ones

--- src/clean/cleaning_missings_new_variables.R
+++ src/clean/cleaning_missings_new_variables.R
@@ -27,54 +27,48 @@ cleaning_missings_new_variables <- function(housing_data = NA) {
                 redc_delivery == del_num
             )
 
-        targets::tar_assert_true(
-            all(
-                unique(delivery_data[[var]]) %in% c(NA)
-            ),
-            msg = glue::glue(
-                "!!! WARNING: ",
-                "Variable {var} is not missing in delivery.",
-                " (Error code: cmnv#1)"
-            )
-        )
-
-        # replace with appropriate missing value
-        if (typeof(housing_data[[var]]) == "character") {
-        housing_data <- housing_data |>
-            dplyr::mutate(
-                !!rlang::sym(var) := dplyr::case_when(
-                    is.na(!!rlang::sym(var)) & redc_delivery == del_num ~ as.character(
-                        helpers_missing_values()[["not_used_anymore"]]
-                    ),
-                    TRUE ~ !!rlang::sym(var)
-                )
-            )
-        } else {
+        # replace missing values with "not_used_anymore" only if all the values
+        # in that delivery for that variable are missing
+        # NOTE: other missing types are set in fixing_remaining_missings.R
+        if (all(is.na(delivery_data[[var]]))) {
+            # replace with appropriate missing value
+            if (typeof(housing_data[[var]]) == "character") {
             housing_data <- housing_data |>
                 dplyr::mutate(
                     !!rlang::sym(var) := dplyr::case_when(
-                        is.na(!!rlang::sym(var)) & redc_delivery == del_num ~ helpers_missing_values()[["not_used_anymore"]],
+                        is.na(!!rlang::sym(var)) & redc_delivery == del_num ~ as.character(
+                            helpers_missing_values()[["not_used_anymore"]]
+                        ),
                         TRUE ~ !!rlang::sym(var)
                     )
                 )
-        }
+            } else {
+                housing_data <- housing_data |>
+                    dplyr::mutate(
+                        !!rlang::sym(var) := dplyr::case_when(
+                            is.na(!!rlang::sym(var)) & redc_delivery == del_num ~ helpers_missing_values()[["not_used_anymore"]],
+                            TRUE ~ !!rlang::sym(var)
+                        )
+                    )
+            }
 
-        # check that the transformation worked
-        num_missings <- length(which(is.na(delivery_data[[var]])))
-        num_replaced <- length(which(
-            housing_data |>
-                dplyr::filter(redc_delivery == del_num) |>
-                dplyr::select(var) == helpers_missing_values()[["not_used_anymore"]]
-        ))
+            # check that the transformation worked
+            num_missings <- length(which(is.na(delivery_data[[var]])))
+            num_replaced <- length(which(
+                housing_data |>
+                    dplyr::filter(redc_delivery == del_num) |>
+                    dplyr::select(var) == helpers_missing_values()[["not_used_anymore"]]
+            ))
 
-        targets::tar_assert_true(
-            num_missings == num_replaced,
-            msg = glue::glue(
-                "!!! WARNING: ",
-                "Variable transformation for {var} did not work.",
-                " (Error code: cmnv#2)"
+            targets::tar_assert_true(
+                num_missings == num_replaced,
+                msg = glue::glue(
+                    "!!! WARNING: ",
+                    "Variable transformation for {var} did not work.",
+                    " (Error code: cmnv#1)"
+                )
             )
-        )
+        }
     }
 
     #--------------------------------------------------
